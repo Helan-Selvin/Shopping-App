13655767d94945058f17dacb3aeab2e2f5c05c3b
Helan-Selvin <helenselvin12343@gmail.com>
2023-06-30 18:18:10 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import Cart from "./Components/Cart";
 import Nav from "./Components/Nav";
 import HomeScreen from "./screens/HomeScreen";
-import ReactGA from 'react-ga'
-
-const TRACKING_ID = '#'
-ReactGA.initialize(TRACKING_ID)
 
 function App() {
   return (
